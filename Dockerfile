415a2ed22448502e0b0e61bc0bef090fd3e6244a
joe <joseph.hanna10@gmail.com>
2019-07-18 17:01:53 -0400

    added uwsgi.ini

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM tiangolo/uwsgi-nginx-flask:python3.6
 
-COPY . /app
-
 WORKDIR /app
+
+COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
 
 ENV LISTEN_PORT 5000
 
 EXPOSE 5000
+
+COPY . /app
--- fleet_watch_app.py
+++ fleet_watch_app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
-
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-	return 'Hello world'
+    var = {"api_version": "1", "key": "pair"}
+    return 'Hello world'
+
+
+@app.route('/login')
+def login():
+    var = {"api_version": "1", "key": "pair"}
+    return json.dumps(var)
+
 
 
 if __name__ == '__main__':
